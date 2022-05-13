352abdcd4fe0717fb5d7230b1b73f4e4c7138f84
Manuel Rueda <rueda.manu@gmail.com>
2022-05-13 12:56:48 +0200

    Source commit

--- run_parallel.sh
+++ run_parallel.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Author: mrueda
+beacon=/pro/beacon/beacon
 #cd vcf
-parallel "/pro/beacon/beacon vcf -n 1 -i {}.vcf.gz  -p ../param.in > {}.vcf.log 2>&1" :::  `ls -1 *[0-9].vcf.gz | cut -d'.' -f1`
+parallel "$beacon vcf -n 1 -i {}.vcf.gz  -p ../param.in > {}.vcf.log 2>&1" :::  `ls -1 *[0-9].vcf.gz | cut -d'.' -f1`
